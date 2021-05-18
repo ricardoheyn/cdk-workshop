aac6490e5ff5d38bf8a4532f0b1287a3bfd811b6
Ricardo Heyn Junior <ricardo@ricardo.net.br>
2021-05-18 23:30:50 +0200

    Update tests

--- test/cdk-workshop.test.ts
+++ test/cdk-workshop.test.ts
@@ -2,20 +2,19 @@ import { expect as expectCDK, haveResource } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as CdkWorkshop from '../lib/cdk-workshop-stack';
 
-test('SQS Queue Created', () => {
+test('Lambda Function Created', () => {
     const app = new cdk.App();
     // WHEN
     const stack = new CdkWorkshop.CdkWorkshopStack(app, 'MyTestStack');
     // THEN
-    expectCDK(stack).to(haveResource("AWS::SQS::Queue",{
-      VisibilityTimeout: 300
+    expectCDK(stack).to(haveResource("AWS::Lambda::Function",{
     }));
 });
 
-test('SNS Topic Created', () => {
+test('API Gateway REST API Created', () => {
   const app = new cdk.App();
   // WHEN
   const stack = new CdkWorkshop.CdkWorkshopStack(app, 'MyTestStack');
   // THEN
-  expectCDK(stack).to(haveResource("AWS::SNS::Topic"));
+  expectCDK(stack).to(haveResource("AWS::ApiGateway::RestApi"));
 });
